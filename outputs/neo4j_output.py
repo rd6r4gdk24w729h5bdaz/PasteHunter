876e5d03fefe323533f2cb75da2e8603a19208eb
root <root@localhost>
2019-01-15 23:05:33 +0100

    Added first draft for neo4j storage

---
+++ outputs/neo4j_output.py
@@ -0,0 +1,44 @@
+from neo4jrestclient.client import GraphDatabase
+from common import parse_config
+import logging
+import re
+
+logger = logging.getLogger('pastehunter')
+config = parse_config()
+
+
+class Neo4jOutput():
+    def __init__(self):
+        # Set up the database connection
+        neo4j_host = config['outputs']['neo4j_output']['neo4j_host']
+        neo4j_port = config['outputs']['neo4j_output']['neo4j_port']
+        neo4j_user = config['outputs']['neo4j_output']['neo4j_user']
+        neo4j_pass = config['outputs']['neo4j_output']['neo4j_pass']
+        self.test = False
+        try:
+            self.db = GraphDatabase(neo4j_host+""+neo4j_port, neo4j_user, neo4j_pass)
+            self.test = True
+        except Exception as e:
+            logger.error(e)
+            raise Exception('Unable to Connect') from None
+
+    def store_paste(self, paste_data):
+        if not self.test:
+            logger.error("Neo4j Enabled, not configured!")
+            return
+
+        # Extract creds from paste
+        cred_counter = 0
+        for line in paste_data['raw_paste'].splitlines():
+            res = self.username_password_regex.match(line)
+            if res:
+                cred = {'username':    res.group("username"),
+                        'password': res.group("password")
+                        }
+                # Insert in DB
+                neo4j_json = re.sub(', "', ', ', re.sub('":', ':', re.sub('({")', '{', cred)))
+                db_insert = "MERGE (:username_password {0})".format(neo4j_json)
+                self.db.query(db_insert)
+                cred_counter += 1
+
+        logger.info("Paste {0} contains {1} username password credentials".format(paste_data["pasteid"], cred_counter))
