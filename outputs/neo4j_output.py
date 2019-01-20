6e47acb07b20486268844f6669fbb44c98b2331e
root <root@localhost>
2019-01-20 21:13:29 +0100

    Fixed last commit

--- outputs/neo4j_output.py
+++ outputs/neo4j_output.py
@@ -93,6 +93,6 @@ class Neo4jOutput():
         except Exception as e:
             if self.debug_mode:
                 logger.debug("FAILLURE at {0} with error {1}".format(paste_data["pasteid"], e))
-                os.system('kill -SIGKILL %d' % os.getpid())
+                os.system('kill -9 %d' % os.getpid())
             else:
                 raise e
